fa51167adee212e0cab034fbdca89c458c078ef3
Tardis Xu <xiaoxubeii@gmail.com>
2014-06-13 13:35:03 +0800

    change Test to test

--- Test/kombu_demo/entity1.py
+++
@@ -1,9 +0,0 @@
-'''
-Created on May 19, 2014
-
-@author: xiaoxubeii
-'''
-from kombu import Exchange, Queue
-
-exchange = Exchange('test', type='direct')
-queue = Queue('q_test', exchange, route_key='test')
